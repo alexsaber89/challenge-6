f6cce4460a0ed54c9e67f0f28bc51f4ea0e39436
Alex Saber <Alex Saber>
2016-08-13 10:50:41 -0500

    refactored both functions using array methods (map & reduce)

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
 // What is the difference between the sum of the squares of the first ten natural numbers,
-// and the square of the sum of the first ten natural numbers?
+  // and the square of the sum of the first ten natural numbers?
 var naturalNums = [1,2,3,4,5,6,7,8,9,10];
 
-function getSum(total, num) {
-  return total + num;
-}
-
 function getSumOfSquares() {
-  console.log("Natural numbers array: ",naturalNums);
-  var squareNums = [];
-  var sumNums;
-  for (var i = 0; i < naturalNums.length; i++) {
-    squareNums.push(Math.pow(naturalNums[i],2));
-  }
-  console.log("Squares of main array: ",squareNums);
-  sumNums = squareNums.reduce(getSum);
-  console.log("Sum of squares of main array: ",sumNums);
+  var squareNums = naturalNums.map(function(num) {
+    return Math.pow(num,2);
+  });
+  var sumNums = squareNums.reduce(function(pv, cv) {
+    return pv + cv;
+  });
   getSquareOfSums(sumNums);
 }
 
 function getSquareOfSums(sumOfSquares) {
-  console.log("Natural numbers array: ",naturalNums);
   var sumOfNats;
-  sumOfNats = naturalNums.reduce(getSum);
-  console.log("Sum of main array: ",sumOfNats);
+  sumOfNats = naturalNums.reduce(function(pv, cv) {
+    return pv + cv;
+  });
   sumOfNats = sumOfNats * sumOfNats;
-  console.log("Square of sums of main array: ",sumOfNats);
   var mainDifference = sumOfNats - sumOfSquares;
   console.log ("Difference between sum of squares and square of sums: ",mainDifference);
 }
 getSumOfSquares();
+
+// var naturalNums = [1,2,3,4,5,6,7,8,9,10];
+
+// function getSum(total, num) {
+//   return total + num;
+// }
+
+// function getSumOfSquares() {
+//   console.log("Natural numbers array: ",naturalNums);
+//   var squareNums = [];
+//   var sumNums;
+//   for (var i = 0; i < naturalNums.length; i++) {
+//     squareNums.push(Math.pow(naturalNums[i],2));
+//   }
+//   console.log("Squares of main array: ",squareNums);
+//   sumNums = squareNums.reduce(getSum);
+//   console.log("Sum of squares of main array: ",sumNums);
+//   getSquareOfSums(sumNums);
+// }
+
+// function getSquareOfSums(sumOfSquares) {
+//   console.log("Natural numbers array: ",naturalNums);
+//   var sumOfNats;
+//   sumOfNats = naturalNums.reduce(getSum);
+//   console.log("Sum of main array: ",sumOfNats);
+//   sumOfNats = sumOfNats * sumOfNats;
+//   console.log("Square of sums of main array: ",sumOfNats);
+//   var mainDifference = sumOfNats - sumOfSquares;
+//   console.log ("Difference between sum of squares and square of sums: ",mainDifference);
+// }
+// getSumOfSquares();
